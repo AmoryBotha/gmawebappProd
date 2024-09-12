c16b5c23e63a98b8293f4325107fd4b4241eedaa
Amory <abotha181@gmail.com>
2024-09-12 11:19:18 +0200

    add portal landing page 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,48 @@ localStorage.getItem(usernameStore);
 
 
 function Home() {
+    
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    const getNotes = () => {
+        api
+            .get("/api/notes/")
+            .then((res) => res.data)
+            .then((data) => {
+                setNotes(data);
+                console.log(data);
+            })
+            .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Note deleted!");
+                else alert("Failed to delete note.");
+                getNotes();
+            })
+            .catch((error) => alert(error));
+    };
+
+    const createNote = (e) => {
+        e.preventDefault();
+        api
+            .post("/api/notes/", { content, title })
+            .then((res) => {
+                if (res.status === 201) alert("Note created!");
+                else alert("Failed to make note.");
+                getNotes();
+            })
+            .catch((err) => alert(err));
+    };
     if(owner === true && contractor === true && trustee === true ){
     return (
         <div>
